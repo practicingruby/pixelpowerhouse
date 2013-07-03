fae73bfd298be0d3a740caa6f7393c9a76304384
Gregory Brown <gregory.t.brown@gmail.com>
2013-07-03 10:29:43 -0400

    horrible and rudimentary code for running multiple workers and conveyors

--- main.lua
+++ main.lua
@@ -12,6 +12,17 @@ function collide(sa, sb)
   return bottomA > topB and topA < bottomB and rightA > leftB and leftA < rightB 
 end
 
+function workerAt(x, y)
+  for i, worker in ipairs(workers) do
+    if x > worker.x - worker.width / 2 and
+       x < worker.x + worker.width / 2 and
+       y > worker.y - worker.height / 2 and
+       y < worker.y + worker.height / 2 then
+       return worker
+     end
+  end
+end
+
 function drawRectangle(rect)
   love.graphics.setColor(rect.color)
   love.graphics.rectangle("fill", rect.x - rect.width / 2, 
@@ -40,7 +51,7 @@ function moveShape(shape, destination)
 end
 
 function love.load()
-  player = {
+  workers = {{
     x = 100,
     y = 100,
     width = 20,
@@ -48,7 +59,18 @@ function love.load()
     moving = false,
     destination = { x = 100, y = 100 },
     color = { 255, 0, 0 }
-  }
+  }, {
+    x = 600,
+    y = 400,
+    width = 20,
+    height = 20,
+    moving = false,
+    destination = { x = 600, y = 400 },
+    color = { 255, 50, 50 }
+  }}
+
+  player = workers[1]
+  player.color = { 255, 100, 100 }
 
   widget = {
     x      = 300,
@@ -84,15 +106,27 @@ function love.load()
     }
   }
 
-  conveyor = {
-    x = 650,
+  conveyors = {{
+    x = 600,
     y = 500,
-    width = 300,
+    width = 200,
+    height = 50,
+    color  = { 33, 33, 33 },
+    dy     = 0,
+    dx     = 1
+  },
+  {
+    x = 700,
+    y = 200,
+    width = 200,
     height = 50,
     color  = { 33, 33, 33 },
     dy     = 0,
     dx     = 1
   }
+
+  }
+
 
   wall = {
     x = 500,
@@ -112,40 +146,58 @@ function love.draw()
     love.graphics.print("You win!", 100,100)
   else
     drawRectangle(wall)
-    drawRectangle(conveyor)
+
+    for i, conveyor in ipairs(conveyors) do
+      drawRectangle(conveyor)
+    end
+
     drawRectangle(machine.body)
     drawRectangle(machine.input)
     drawRectangle(machine.output)
 
-    drawRectangle(player)
+    for i, worker in ipairs(workers) do
+      drawRectangle(worker)
+    end
+
     drawRectangle(widget)
   end
 end
 
 function love.update(dt)
-  if collide(player, wall) then
-    moveShape(player, { x = wall.x - 25, y = player.y })
-  else
-    moveShape(player, player.destination)
-  end
+  for i, worker in ipairs(workers) do
+    if collide(worker, wall) then
+      moveShape(worker, { x = worker.x - 25, y = worker.y })
+    else
+      moveShape(worker, worker.destination)
+    end
 
-  if collide(widget, conveyor) then
-    widget.x = widget.x + conveyor.dx
-    widget.y = conveyor.y
-  elseif collide(widget, machine.input) then
-    widget.x = machine.output.x
-    widget.y = machine.output.y
+    for i, conveyor in ipairs(conveyors) do
+      if collide(widget, conveyor) then
+        widget.x = widget.x + conveyor.dx
+        widget.y = conveyor.y
+      elseif collide(widget, machine.input) then
+        widget.x = machine.output.x
+        widget.y = machine.output.y
 
-    widget.color = {0, 255, 0}
-
-  elseif collide(widget, player) then
-    widget.x = player.x
-    widget.y = player.y
+        widget.color = {0, 255, 0}
+      elseif collide(widget, worker) then
+        widget.x = worker.x
+        widget.y = worker.y
+      end
+    end
   end
 end
 
 function love.mousepressed(x, y, button)
   if button == "l" then
-    player.destination = { x = x, y = y }
+    if workerAt(x, y) then
+      player.color = { 255, 50, 50 }
+
+      player = workerAt(x, y)
+      player.color = { 255, 150, 150 }
+      player.destination = { x = player.x, y = player.y }
+    else
+      player.destination = { x = x, y = y }
+    end
   end
 end
